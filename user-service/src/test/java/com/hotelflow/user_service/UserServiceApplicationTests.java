dc06079fd92ea961ba12181e61f5f844ccaec299
THETOXICSIDEOFME <rayen.stark@protonmail.com>
2026-04-01 12:49:04 +0100

    initial commit

---
+++ user-service/src/test/java/com/hotelflow/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelflow.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
